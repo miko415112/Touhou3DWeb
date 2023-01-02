75089a76a6dbdd66d66e4de03597b054dc8136ff
miko415112 <chocolate80114@gmail.com>
2023-01-03 00:49:44 +0800

    modify file structure

--- frontend/src/components/resource.js
+++ frontend/src/components/resource.js
@@ -12,12 +12,12 @@ const textureLoader = new TextureLoader();
 
 export const getSkyboxTextureArray = () => {
   const textureArray = cubeTextureLoader.load([
-    require('../resource/skybox/corona_ft.png'),
-    require('../resource/skybox/corona_bk.png'),
-    require('../resource/skybox/corona_up.png'),
-    require('../resource/skybox/corona_dn.png'),
-    require('../resource/skybox/corona_rt.png'),
-    require('../resource/skybox/corona_lf.png'),
+    require('./resource/skybox/corona_ft.png'),
+    require('./resource/skybox/corona_bk.png'),
+    require('./resource/skybox/corona_up.png'),
+    require('./resource/skybox/corona_dn.png'),
+    require('./resource/skybox/corona_rt.png'),
+    require('./resource/skybox/corona_lf.png'),
   ]);
 
   textureArray.encoding = sRGBEncoding;
@@ -26,7 +26,7 @@ export const getSkyboxTextureArray = () => {
 };
 
 export const getGroundTexture = () => {
-  const texture = textureLoader.load(require('../resource/dirt.jpg'));
+  const texture = textureLoader.load(require('./resource/dirt.jpg'));
   texture.wrapS = RepeatWrapping;
   texture.wrapT = RepeatWrapping;
   texture.repeat.set(100, 100);
@@ -35,46 +35,46 @@ export const getGroundTexture = () => {
 
 export const getLargeBulletTexture = () => {
   const texture = textureLoader.load(
-    require('../resource/bullet/largeBullet.png')
+    require('./resource/bullet/largeBullet.png')
   );
   return texture;
 };
 
 export const getNormalBulletTexture = () => {
   const texture = textureLoader.load(
-    require('../resource/bullet/normalBullet.png')
+    require('./resource/bullet/normalBullet.png')
   );
   return texture;
 };
 
 export const getSplitBulletTexture = () => {
   const texture = textureLoader.load(
-    require('../resource/bullet/splitBullet.png')
+    require('./resource/bullet/splitBullet.png')
   );
   return texture;
 };
 
 export const getColorfulBulletTextureArray = () => {
-  const red = textureLoader.load(require('../resource/bullet/redBullet.png'));
+  const red = textureLoader.load(require('./resource/bullet/redBullet.png'));
   const purple = textureLoader.load(
-    require('../resource/bullet/purpleBullet.png')
+    require('./resource/bullet/purpleBullet.png')
   );
   const green = textureLoader.load(
-    require('../resource/bullet/greenBullet.png')
+    require('./resource/bullet/greenBullet.png')
   );
   const yellow = textureLoader.load(
-    require('../resource/bullet/yellowBullet.png')
+    require('./resource/bullet/yellowBullet.png')
   );
   return [red, purple, green, yellow];
 };
 
 export const getPowerTexture = () => {
-  const powerTexture = textureLoader.load(require('../resource/powerItem.png'));
+  const powerTexture = textureLoader.load(require('./resource/powerItem.png'));
   return powerTexture;
 };
 
 export const RemiliaModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Remilia/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Remilia/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <group position={[0, 2, 0]}>
@@ -88,7 +88,7 @@ export const RemiliaModel = (props) => {
 };
 
 export const KoishiModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Koishi/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Koishi/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <group position={[0, -135, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.8}>
@@ -106,7 +106,7 @@ export const KoishiModel = (props) => {
 };
 
 export const SuwakoModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Suwako/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Suwako/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <group position={[0, -10, 0]} rotation={[0, Math.PI / 2, 0]}>
@@ -120,7 +120,7 @@ export const SuwakoModel = (props) => {
 };
 
 export const MeilingModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Meiling/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Meiling/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <mesh
@@ -134,7 +134,7 @@ export const MeilingModel = (props) => {
 };
 
 export const SakuyaModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Sakuya/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Sakuya/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <group scale={1}>
@@ -151,7 +151,7 @@ export const SakuyaModel = (props) => {
 };
 
 export const SanaeModel = (props) => {
-  const { nodes, materials } = useGLTF('/model/Sanae/Scene.gltf');
+  const { nodes, materials } = useGLTF('model/Sanae/scene.gltf');
   return (
     <group {...props} dispose={null}>
       <mesh
@@ -165,14 +165,14 @@ export const SanaeModel = (props) => {
   );
 };
 
-export const roomBackgroundImage = require('../resource/roomBackground.jpg');
-export const homeBackgroundImage = require('../resource/background.png');
-export const nameBorderImage = require('../resource/nameBorder.png');
-export const charaBorderImage = require('../resource/charaBorder.png');
+export const roomBackgroundImage = require('./resource/roomBackground.jpg');
+export const homeBackgroundImage = require('./resource/background.png');
+export const nameBorderImage = require('./resource/nameBorder.png');
+export const charaBorderImage = require('./resource/charaBorder.png');
 
-useGLTF.preload('/model/Remilia/Scene.gltf');
-useGLTF.preload('/model/Koishi/Scene.gltf');
-useGLTF.preload('/model/Suwako/Scene.gltf');
-useGLTF.preload('/model/Meiling/Scene.gltf');
-useGLTF.preload('/model/Sakuya/Scene.gltf');
-useGLTF.preload('/model/Sanae/Scene.gltf');
+useGLTF.preload('model/Remilia/scene.gltf');
+useGLTF.preload('model/Koishi/scene.gltf');
+useGLTF.preload('model/Suwako/scene.gltf');
+useGLTF.preload('model/Meiling/scene.gltf');
+useGLTF.preload('model/Sakuya/scene.gltf');
+useGLTF.preload('model/Sanae/scene.gltf');
