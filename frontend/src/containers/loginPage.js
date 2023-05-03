c98dbbb9adf960c3f9045bfc0bd9db17111a08bc
miko415112 <chocolate80114@gmail.com>
2023-05-03 20:53:39 +0800

    update README and game info

--- frontend/src/containers/loginPage.js
+++ frontend/src/containers/loginPage.js
@@ -4,17 +4,20 @@ import { googleLoginImage, introImage } from "../components/resource";
 import { network } from "./hooks/network";
 import { useUser } from "./hooks/context";
 import { useEffect } from "react";
+
 const LoginPageWrapper = styled.div`
   display: flex;
   width: 1200px;
   height: 675px;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `;
 
 const ContentWrapper = styled.div`
   display: flex;
   gap: 40px;
+  padding: 50px;
   img {
     width: 250px;
   }
@@ -40,6 +43,114 @@ const GoogleButtonWrapper = styled.button`
     cursor: pointer;
   }
 `;
+
+const GameInfo = () => {
+  return (
+    <div>
+      <h2>Controls</h2>
+      <ul>
+        <li>
+          Menu Operation: Use the up and down arrow keys to change the option,
+          press Z to confirm selection.
+        </li>
+        <li>
+          Room Operation: Use the up and down arrow keys to change the option,
+          which includes start, quit, invite, room ID, and character. When in
+          the "Character" option, arrows will appear on both sides of the
+          character frame. Pressing the left and right arrow keys will change
+          the selected character. All players must press Z to be ready before
+          the game starts.
+        </li>
+        <li>
+          Game Operation: Use W/S to move up and down, A/D to rotate left and
+          right, the space bar to move forward, and H/J/K/L to shoot bullets.
+          (Note: Only the ghost can shoot four types of bullets, while others
+          can only use H.) Press ESC to open the menu, Shift to slow down the
+          character's movement speed.
+        </li>
+      </ul>
+      <h2>Game Rules</h2>
+      <ul>
+        <li>
+          Each player has three lives, and the field is divided into two areas.
+          The ghost is on one side, and the remaining players are on the other
+          side. The movement range is limited, and players cannot move beyond
+          the boundary.
+        </li>
+        <li>
+          When hit by a bullet, players will have three seconds of
+          invincibility.
+        </li>
+        <li>Once a player dies, they can no longer shoot bullets.</li>
+        <li>
+          To end the game, press ESC to open the menu, and then leave the game
+          without refreshing.
+        </li>
+      </ul>
+      <h2>Reference</h2>
+      <h3>Image</h3>
+      <ul>
+        <li>
+          <a href="https://www.spriters-resource.com/fullview/120461/">
+            https://www.spriters-resource.com/fullview/120461/
+          </a>
+        </li>
+        <li>
+          <a href="https://www.goodfon.com/wallpaper/moon-cosmos-sci-fi-art.html">
+            https://www.goodfon.com/wallpaper/moon-cosmos-sci-fi-art.html
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com/pin/676877018993327227/">
+            https://www.pinterest.com/pin/676877018993327227/
+          </a>
+        </li>
+      </ul>
+
+      <h3>Model</h3>
+      <ul>
+        <li>
+          <a href="https://sketchfab.com/3d-models/remilia-scarlet-fumo-3d-scan-e45fe2f126c84c2d84964468746ce257">
+            https://sketchfab.com/3d-models/remilia-scarlet-fumo-3d-scan-e45fe2f126c84c2d84964468746ce257
+          </a>
+        </li>
+        <li>
+          <a href="https://sketchfab.com/3d-models/project-suwako-moriya-fumo-2cb48aeeb1d94b27960b416fbb368929">
+            https://sketchfab.com/3d-models/project-suwako-moriya-fumo-2cb48aeeb1d94b27960b416fbb368929
+          </a>
+        </li>
+        <li>
+          <a href="https://sketchfab.com/3d-models/project-koishi-komeiji-fumo-37a56b489b5c4440bbabb7b7777036f6">
+            https://sketchfab.com/3d-models/project-koishi-komeiji-fumo-37a56b489b5c4440bbabb7b7777036f6
+          </a>
+        </li>
+        <li>
+          <a href="https://sketchfab.com/3d-models/project-sanae-kochiya-fumo-49518274d9b340d6b689298ba2ecc8a8">
+            https://sketchfab.com/3d-models/project-sanae-kochiya-fumo-49518274d9b340d6b689298ba2ecc8a8
+          </a>
+        </li>
+        <li>
+          <a href="https://sketchfab.com/3d-models/project-hong-meiling-fumo-8c7a286931164bf693b524679d85db99">
+            https://sketchfab.com/3d-models/project-hong-meiling-fumo-8c7a286931164bf693b524679d85db99
+          </a>
+        </li>
+        <li>
+          <a href="https://sketchfab.com/3d-models/scuffed-sakuya-fumo-0d83abc11bfa434a96cedeb453e308b0">
+            https://sketchfab.com/3d-models/scuffed-sakuya-fumo-0d83abc11bfa434a96cedeb453e308b0
+          </a>
+        </li>
+      </ul>
+      <h3>Sound</h3>
+      <ul>
+        <li>
+          <a href="https://www.sounds-resource.com/pc_computer/touhoukoumakyoutheembodimentofscarletdevil/sound/327/">
+            https://www.sounds-resource.com/pc_computer/touhoukoumakyoutheembodimentofscarletdevil/sound/327/
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const LoginPage = () => {
   const { signIn, setSignIn, setProfile } = useUser();
@@ -84,6 +195,7 @@ const LoginPage = () => {
             </GoogleButtonWrapper>
           </LoginWrapper>
         </ContentWrapper>
+        <GameInfo />
       </LoginPageWrapper>
     </>
   );
